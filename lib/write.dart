d0993da2847f8cecb4f7b0c0c28a4ec3a970041e
rohmi <rohmi@ipactory.com>
2021-11-04 01:27:18 +0900

    fix: 네비게이션 오류 수정

--- lib/write.dart
+++ lib/write.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'data/todo.dart';
 
 class TodoWritePage extends StatefulWidget {
-  TodoWritePage({Key? key, required this.todo}) : super(key: key);
   final Todo todo;
+
+  TodoWritePage({Key? key, required this.todo}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -15,40 +16,19 @@ class TodoWritePage extends StatefulWidget {
 }
 
 class _TodoWritePageState extends State<TodoWritePage> {
-  TextEditingController nameController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
           TextButton(
-            onPressed: () {
-              // todo: 페이지 저장시 사용
-            },
-            child: Text("저장", style: TextStyle(color: Colors.white)),
-          )
+              onPressed: () {},
+              child: Text(
+                "저장",
+                style: TextStyle(color: Colors.white),
+              ))
         ],
       ),
-      body: ListView.builder(itemBuilder: (ctx, idx) {
-        if (idx == 0) {
-          return Container(child: Text("제목"));
-        } else if (idx == 1) {
-          return Container(
-              child: TextField(
-            controller: nameController,
-          ));
-        } else if (idx == 2) {
-          return Container(
-              child: Row(
-            children: [
-              Text("색상"),
-              Container(width: 10, height: 10, color: Color(widget.todo.color))
-            ],
-          ));
-        }
-        return Container();
-      }),
     );
   }
 }
